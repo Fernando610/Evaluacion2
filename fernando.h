d9ea4aa1eae68c544ef0421e250bb7277bf672eb
Fernando Baena <55531240+Fernando610@users.noreply.github.com>
2020-02-20 09:35:05 +0100

    Add files via upload

---
+++ fernando.h
@@ -0,0 +1,9 @@
+int devuelve_longitud(char aux[]){
+    int cont=0;
+    int longitud=0;
+    while(aux[cont]!='\0'){
+                           longitud++;
+                           cont++;
+                           }
+return(longitud);
+}
